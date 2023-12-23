542967d66d9d6c23931fa6d08cfd22f5667a0c02
Ostap4i <137522973+Ostap4i@users.noreply.github.com>
2023-12-23 11:22:26 +0200

    Add files via upload

---
+++ DZ/DZ.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+class Character {
+private:
+    char symbol;
+    int size;
+    std::string font;
+
+public:
+    Character(char symbol, int size, std::string font) : symbol(symbol), size(size), font(font) {}
+
+    void draw() {
+\
+        std::cout << "Character: " << symbol << ", Size: " << size << ", Font: " << font << std::endl;
+    }
+
+
+};
+
+
+class CharacterFactory {
+public:
+
+    std::unordered_map<char, Character*> characters;
+
+
+    Character* getCharacter(char symbol, int size, std::string font) {
+        if (characters.find(symbol) != characters.end()) {
+       
+            return characters[symbol];
+        }
+        else {
+            Character* newCharacter = new Character(symbol, size, font);
+            characters[symbol] = newCharacter;
+            return newCharacter;
+        }
+    }
+};
+
+int main() {
+  
+    CharacterFactory characterFactory;
+
+    Character* charA = characterFactory.getCharacter('A', 12, "Arial");
+    charA->draw();
+
+    Character* charB = characterFactory.getCharacter('B', 14, "Times New Roman");
+    charB->draw();
+
+    Character* charA2 = characterFactory.getCharacter('A', 12, "Arial");  
+    charA2->draw();
+
+
+    return 0;
+}
---
+++ проксі/проксі.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+
+
+class Document {
+public:
+    virtual std::string readDocument() const = 0;
+    virtual void writeDocument(const std::string& content) = 0;
+};
+
+class ConcreteDocument : public Document {
+private:
+    std::string content;
+
+public:
+    std::string readDocument() const override {
+        return content;
+    }
+
+    void writeDocument(const std::string& newContent) override {
+        content = newContent;
+    }
+};
+
+
+class DocumentAccess {
+public:
+    virtual std::string readDocument() const = 0;
+    virtual void writeDocument(const std::string& content) = 0;
+};
+
+
+class RealDocumentAccess : public DocumentAccess {
+private:
+    ConcreteDocument document;
+
+public:
+    std::string readDocument() const override {
+        return document.readDocument();
+    }
+
+    void writeDocument(const std::string& content) override {
+        document.writeDocument(content);
+    }
+};
+
+
+class ProxyDocumentAccess : public DocumentAccess {
+private:
+    RealDocumentAccess realAccess;
+    std::unordered_map<std::string, std::string> accessControl; 
+
+public:
+    std::string readDocument() const override {
+        
+        if (accessControl.find("allowed_document") != accessControl.end()) {
+            return realAccess.readDocument();
+        }
+        else {
+            return "Access denied.";
+        }
+    }
+
+    void writeDocument(const std::string& content) override {
+        if (accessControl.find("allowed_document") != accessControl.end()) {
+            realAccess.writeDocument(content);
+        }
+        else {
+            std::cout << "Write access denied." << std::endl;
+        }
+    }
+};
+
+int main() {
+    RealDocumentAccess realAccess;
+    ProxyDocumentAccess proxyAccess;
+
+
+    std::cout << "Real Document Access:" << std::endl;
+    realAccess.writeDocument("This is a real document.");
+    std::cout << "Read document: " << realAccess.readDocument() << std::endl;
+
+
+    std::cout << "\nProxy Document Access:" << std::endl;
+    proxyAccess.writeDocument("This is a proxy document.");
+    std::cout << "Read document: " << proxyAccess.readDocument() << std::endl;
+
+    return 0;
+}
